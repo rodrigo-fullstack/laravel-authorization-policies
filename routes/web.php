9acb0ae28ba18aa18efd980adeb557936f9a8d01
rodrigo-fullstack <rodrigopereirafaria9@gmail.com>
2025-04-28 13:46:17 -0300

    style: Delete lines

--- routes/web.php
+++ routes/web.php
@@ -3,27 +3,18 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PostController;
 use App\Livewire\Login;
-use App\Models\Post;
-use App\Models\User;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->group(function () {
-
-    
     Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
-    
     // Route::get('/')
-
-    Route::get('/', [PostController::class, 'posts']   
+    Route::get('/', [PostController::class, 'posts']
     )->name('home');
-
-    Route::get('/posts/update/{id}', [PostController::class, 'update']   
+    Route::get('/posts/update/{id}', [PostController::class, 'update']
     )->name('update');
-    Route::get('/posts/delete/{id}', [PostController::class, 'delete']   
+    Route::get('/posts/delete/{id}', [PostController::class, 'delete']
     )->name('delete');
-
-    Route::get('/posts/create', [PostController::class, 'create']   
+    Route::get('/posts/create', [PostController::class, 'create']
     )->name('create');
 });
 
